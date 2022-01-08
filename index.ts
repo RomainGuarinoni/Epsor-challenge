9a10ecf0cf463ad6f428c4b8df7fc88ba1c07108
RomainGuarinoni <romain.guar91@gmail.com>
2022-01-08 15:59:43 +0100

    feat(resolver): create a Hello world resolver

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-export {};
+import 'reflect-metadata';
+import Express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema, Resolver, Query } from 'type-graphql';
+
+const PORT = process.env.PORT || 3000;
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async HelloWorld() {
+    return 'Hello World';
+  }
+}
+
+async function start() {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  try {
+    app.listen(PORT, () => {
+      console.log('🔌 Server is listening on http://localhost:' + PORT);
+      console.log(`📕 graphql visualizer on http://localhost:${PORT}/graphql`);
+    });
+  } catch (err) {
+    console.log(
+      '❌ Error happened while starting the server',
+      JSON.stringify(err),
+    );
+  }
+}
+
+start();
